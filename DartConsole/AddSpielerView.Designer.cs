cd894086f3a8dc538864f692ab8f50fd1c9511ff
edhor1608 <rohde.jonas@freenet.de>
2018-06-24 20:17:49 +0200

    Tab Index in AddSpieler angepasst weitere Fenstereinstellungen

--- DartConsole/AddSpielerView.Designer.cs
+++ DartConsole/AddSpielerView.Designer.cs
@@ -28,32 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddSpielerView));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tBx_username = new System.Windows.Forms.TextBox();
+            this.tBx_Passwort_Wdh = new System.Windows.Forms.TextBox();
+            this.tBx_Passwort = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.btn_OK = new System.Windows.Forms.Button();
             this.lbl_FirstName = new System.Windows.Forms.Label();
             this.lbl_Nachname = new System.Windows.Forms.Label();
-            this.lbl_Geburtsdatum = new System.Windows.Forms.Label();
-            this.lbl_eMail = new System.Windows.Forms.Label();
             this.tBx_eMail = new System.Windows.Forms.TextBox();
             this.tBx_Geburtsdatum = new System.Windows.Forms.TextBox();
             this.tBx_LastName = new System.Windows.Forms.TextBox();
             this.tBx_FirstName = new System.Windows.Forms.TextBox();
+            this.lbl_eMail = new System.Windows.Forms.Label();
+            this.lbl_Geburtsdatum = new System.Windows.Forms.Label();
             this.lbl_Passwort = new System.Windows.Forms.Label();
-            this.tBx_Passwort = new System.Windows.Forms.TextBox();
             this.lbl_Passwort_Wdh = new System.Windows.Forms.Label();
-            this.tBx_Passwort_Wdh = new System.Windows.Forms.TextBox();
             this.lbl_username = new System.Windows.Forms.Label();
-            this.tBx_username = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.tBx_username, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.tBx_Passwort_Wdh, 1, 5);
             this.tableLayoutPanel1.Controls.Add(this.tBx_Passwort, 1, 4);
@@ -69,200 +68,106 @@
             this.tableLayoutPanel1.Controls.Add(this.lbl_Passwort, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.lbl_Passwort_Wdh, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.lbl_username, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 8;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(468, 383);
-            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tBx_username
+            // 
+            resources.ApplyResources(this.tBx_username, "tBx_username");
+            this.tBx_username.BackColor = System.Drawing.Color.White;
+            this.tBx_username.Name = "tBx_username";
+            // 
+            // tBx_Passwort_Wdh
+            // 
+            resources.ApplyResources(this.tBx_Passwort_Wdh, "tBx_Passwort_Wdh");
+            this.tBx_Passwort_Wdh.BackColor = System.Drawing.Color.White;
+            this.tBx_Passwort_Wdh.Name = "tBx_Passwort_Wdh";
+            this.tBx_Passwort_Wdh.UseSystemPasswordChar = true;
+            // 
+            // tBx_Passwort
+            // 
+            resources.ApplyResources(this.tBx_Passwort, "tBx_Passwort");
+            this.tBx_Passwort.BackColor = System.Drawing.Color.White;
+            this.tBx_Passwort.Name = "tBx_Passwort";
+            this.tBx_Passwort.UseSystemPasswordChar = true;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 225F));
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
             this.tableLayoutPanel2.Controls.Add(this.btn_OK, 1, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(238, 334);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(226, 44);
-            this.tableLayoutPanel2.TabIndex = 0;
             // 
             // btn_OK
             // 
-            this.btn_OK.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_OK.Location = new System.Drawing.Point(5, 5);
-            this.btn_OK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            resources.ApplyResources(this.btn_OK, "btn_OK");
             this.btn_OK.Name = "btn_OK";
-            this.btn_OK.Size = new System.Drawing.Size(217, 34);
-            this.btn_OK.TabIndex = 0;
-            this.btn_OK.Text = "OK";
             this.btn_OK.UseVisualStyleBackColor = true;
             this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
             // 
             // lbl_FirstName
             // 
-            this.lbl_FirstName.AutoSize = true;
-            this.lbl_FirstName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_FirstName.Location = new System.Drawing.Point(4, 47);
-            this.lbl_FirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            resources.ApplyResources(this.lbl_FirstName, "lbl_FirstName");
             this.lbl_FirstName.Name = "lbl_FirstName";
-            this.lbl_FirstName.Size = new System.Drawing.Size(226, 47);
-            this.lbl_FirstName.TabIndex = 1;
-            this.lbl_FirstName.Text = "Vorname";
             // 
             // lbl_Nachname
             // 
-            this.lbl_Nachname.AutoSize = true;
-            this.lbl_Nachname.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_Nachname.Location = new System.Drawing.Point(4, 94);
-            this.lbl_Nachname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            resources.ApplyResources(this.lbl_Nachname, "lbl_Nachname");
             this.lbl_Nachname.Name = "lbl_Nachname";
-            this.lbl_Nachname.Size = new System.Drawing.Size(226, 47);
-            this.lbl_Nachname.TabIndex = 2;
-            this.lbl_Nachname.Text = "Nachname";
-            // 
-            // lbl_Geburtsdatum
-            // 
-            this.lbl_Geburtsdatum.AutoSize = true;
-            this.lbl_Geburtsdatum.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_Geburtsdatum.Location = new System.Drawing.Point(4, 282);
-            this.lbl_Geburtsdatum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbl_Geburtsdatum.Name = "lbl_Geburtsdatum";
-            this.lbl_Geburtsdatum.Size = new System.Drawing.Size(226, 47);
-            this.lbl_Geburtsdatum.TabIndex = 3;
-            this.lbl_Geburtsdatum.Text = "Geburtsdatum";
-            // 
-            // lbl_eMail
-            // 
-            this.lbl_eMail.AutoSize = true;
-            this.lbl_eMail.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_eMail.Location = new System.Drawing.Point(4, 141);
-            this.lbl_eMail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbl_eMail.Name = "lbl_eMail";
-            this.lbl_eMail.Size = new System.Drawing.Size(226, 47);
-            this.lbl_eMail.TabIndex = 4;
-            this.lbl_eMail.Text = "E-Mail";
             // 
             // tBx_eMail
             // 
+            resources.ApplyResources(this.tBx_eMail, "tBx_eMail");
             this.tBx_eMail.BackColor = System.Drawing.Color.White;
-            this.tBx_eMail.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_eMail.Location = new System.Drawing.Point(238, 146);
-            this.tBx_eMail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tBx_eMail.Name = "tBx_eMail";
-            this.tBx_eMail.Size = new System.Drawing.Size(226, 26);
-            this.tBx_eMail.TabIndex = 5;
             // 
             // tBx_Geburtsdatum
             // 
-            this.tBx_Geburtsdatum.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_Geburtsdatum.Location = new System.Drawing.Point(238, 287);
-            this.tBx_Geburtsdatum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            resources.ApplyResources(this.tBx_Geburtsdatum, "tBx_Geburtsdatum");
             this.tBx_Geburtsdatum.Name = "tBx_Geburtsdatum";
             this.tBx_Geburtsdatum.ReadOnly = true;
-            this.tBx_Geburtsdatum.Size = new System.Drawing.Size(226, 26);
-            this.tBx_Geburtsdatum.TabIndex = 6;
             this.tBx_Geburtsdatum.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Geburstsdatum_Aendern);
             // 
             // tBx_LastName
             // 
-            this.tBx_LastName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_LastName.Location = new System.Drawing.Point(238, 99);
-            this.tBx_LastName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            resources.ApplyResources(this.tBx_LastName, "tBx_LastName");
             this.tBx_LastName.Name = "tBx_LastName";
-            this.tBx_LastName.Size = new System.Drawing.Size(226, 26);
-            this.tBx_LastName.TabIndex = 7;
             // 
             // tBx_FirstName
             // 
-            this.tBx_FirstName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_FirstName.Location = new System.Drawing.Point(238, 52);
-            this.tBx_FirstName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            resources.ApplyResources(this.tBx_FirstName, "tBx_FirstName");
             this.tBx_FirstName.Name = "tBx_FirstName";
-            this.tBx_FirstName.Size = new System.Drawing.Size(226, 26);
-            this.tBx_FirstName.TabIndex = 8;
+            // 
+            // lbl_eMail
+            // 
+            resources.ApplyResources(this.lbl_eMail, "lbl_eMail");
+            this.lbl_eMail.Name = "lbl_eMail";
+            // 
+            // lbl_Geburtsdatum
+            // 
+            resources.ApplyResources(this.lbl_Geburtsdatum, "lbl_Geburtsdatum");
+            this.lbl_Geburtsdatum.Name = "lbl_Geburtsdatum";
             // 
             // lbl_Passwort
             // 
-            this.lbl_Passwort.AutoSize = true;
-            this.lbl_Passwort.Location = new System.Drawing.Point(3, 188);
+            resources.ApplyResources(this.lbl_Passwort, "lbl_Passwort");
             this.lbl_Passwort.Name = "lbl_Passwort";
-            this.lbl_Passwort.Size = new System.Drawing.Size(74, 20);
-            this.lbl_Passwort.TabIndex = 9;
-            this.lbl_Passwort.Text = "Passwort";
-            // 
-            // tBx_Passwort
-            // 
-            this.tBx_Passwort.BackColor = System.Drawing.Color.White;
-            this.tBx_Passwort.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_Passwort.Location = new System.Drawing.Point(238, 193);
-            this.tBx_Passwort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tBx_Passwort.Name = "tBx_Passwort";
-            this.tBx_Passwort.Size = new System.Drawing.Size(226, 26);
-            this.tBx_Passwort.TabIndex = 10;
-            this.tBx_Passwort.UseSystemPasswordChar = true;
             // 
             // lbl_Passwort_Wdh
             // 
-            this.lbl_Passwort_Wdh.AutoSize = true;
-            this.lbl_Passwort_Wdh.Location = new System.Drawing.Point(3, 235);
+            resources.ApplyResources(this.lbl_Passwort_Wdh, "lbl_Passwort_Wdh");
             this.lbl_Passwort_Wdh.Name = "lbl_Passwort_Wdh";
-            this.lbl_Passwort_Wdh.Size = new System.Drawing.Size(115, 20);
-            this.lbl_Passwort_Wdh.TabIndex = 11;
-            this.lbl_Passwort_Wdh.Text = "Passwort Wdh.";
-            // 
-            // tBx_Passwort_Wdh
-            // 
-            this.tBx_Passwort_Wdh.BackColor = System.Drawing.Color.White;
-            this.tBx_Passwort_Wdh.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_Passwort_Wdh.Location = new System.Drawing.Point(238, 240);
-            this.tBx_Passwort_Wdh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tBx_Passwort_Wdh.Name = "tBx_Passwort_Wdh";
-            this.tBx_Passwort_Wdh.Size = new System.Drawing.Size(226, 26);
-            this.tBx_Passwort_Wdh.TabIndex = 12;
-            this.tBx_Passwort_Wdh.UseSystemPasswordChar = true;
             // 
             // lbl_username
             // 
-            this.lbl_username.AutoSize = true;
-            this.lbl_username.Location = new System.Drawing.Point(3, 0);
+            resources.ApplyResources(this.lbl_username, "lbl_username");
             this.lbl_username.Name = "lbl_username";
-            this.lbl_username.Size = new System.Drawing.Size(83, 20);
-            this.lbl_username.TabIndex = 13;
-            this.lbl_username.Text = "Username";
-            // 
-            // tBx_username
-            // 
-            this.tBx_username.BackColor = System.Drawing.Color.White;
-            this.tBx_username.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tBx_username.Location = new System.Drawing.Point(238, 5);
-            this.tBx_username.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tBx_username.Name = "tBx_username";
-            this.tBx_username.Size = new System.Drawing.Size(226, 26);
-            this.tBx_username.TabIndex = 14;
             // 
             // AddSpielerView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AcceptButton = this.btn_OK;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 383);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AddSpielerView";
-            this.Text = "AddSpielerView";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
